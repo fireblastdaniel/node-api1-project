93773f36ffd8ad3e2915cdc2170ec5eba88b7851
Daniel Prue <42305970+fireblastdaniel@users.noreply.github.com>
2020-03-09 12:48:44 -0700

    finished specs for POST /api/users endpoint

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require('express');
+const shortid = require('shortid');
 
 const server = express();
+
+let users = [];
 
 server.use(express.json());
 
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'running... ' });
+});
+
+server.post('/api/users', (req, res) => {
+  const userInfo = req.body;
+  if (!userInfo.name || !userInfo.bio) {
+    res
+      .status(400)
+      .json({ errorMessage: 'Please provide name and bio for the user.' });
+  } else {
+    userInfo.id = shortid.generate();
+    users.push(userInfo);
+    users.find(user => user.id === userInfo.id)
+      ? res.status(201).json(userInfo)
+      : res.status(500).json({
+          errorMessage: 'There was an error saving the user to the database'
+        });
+  }
+});
+
+server.get('/api/users', (req, res) => {
+  res.status(200).json(userInfo);
 });
 
 const PORT = 5000;
